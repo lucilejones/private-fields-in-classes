9b8d9ae9e463b7214971909d2ac063a68bac0d52
Lucile Jones <jones.lucile@gmail.com>
2023-09-11 13:16:03 -0600

    completed and passed tests

--- person.js
+++ person.js
@@ -1,5 +1,18 @@
 class Person {
-  // Write code here
+  #id;
+
+  constructor() {
+    this.#id = Math.random();
+  }
+
+  getID() {
+    return this.#id;
+  }
 }
+
+const person1 = new Person();
+// console.log(person1.id);
+// the console.log will print undefined
+person1.getID();
 
 module.exports = Person;
